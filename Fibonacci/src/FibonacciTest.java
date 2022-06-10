197c900e73b7789ec06ae68f3f809d2d512a1d6d
SrJean97 <jheancarlos1997123@gmail.com>
2022-06-10 11:05:48 -0500

    Implementación del código y 5 pruebas realizadas

--- Fibonacci/src/FibonacciTest.java
+++ Fibonacci/src/FibonacciTest.java
@@ -2,6 +2,17 @@
 public class FibonacciTest {
 
 	public static int SerieFibonacci(int posicion){
-		return 1;
+		
+		int num1 = 0;
+		int num2 = 1;
+		
+		for (int i = 0; i < posicion; i++)
+		{
+		    int temp = num1;
+		    num1 = num2;
+		    num2 = temp + num2;
+		}
+		return num1;
+		
 	}
 }
